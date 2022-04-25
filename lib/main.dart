c64ecf8fe96999ef8dd1d5126e6fc5db000e199c
manav <manavkhadka2004@gmail.com>
2022-04-25 22:21:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: "Attendance app",
+    ));
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material();
+  }
+}
